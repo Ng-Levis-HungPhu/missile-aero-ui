6bb5bc7bf3b20552c5687faf94a973065b60d8e3
Ng-Levis-HungPhu <hungphunguyen.3103@gmail.com>
2025-06-24 05:55:20 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,115 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+from tensorflow import keras
+import tensorflow as tf
+
+import numpy as np
+import os
+
+import pickle
+
+from sklearn.preprocessing import MinMaxScaler
+
+app = Flask(__name__)
+CORS(app)
+
+MODEL_DIR = "1. SAVING MODELS"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = request.get_json()
+        mode = data.get("mode", "").strip()
+
+        mach = float(data['mach'])
+        aoa = float(data['aoa'])
+        ln = float(data['ln'])
+        swept = float(data['swept'])
+        lln = float(data['lln'])
+
+        if mode == "NASA":
+            model_cl_path = os.path.join(MODEL_DIR, "NASA_cl.h5")
+            model_cd_path = os.path.join(MODEL_DIR, "NASA_cd.h5")
+            scaler_path = os.path.join(MODEL_DIR, "NASA.pkl")
+            
+            model_cl = tf.keras.models.load_model(model_cl_path)
+            model_cd = tf.keras.models.load_model(model_cd_path)
+
+            with open(scaler_path,'rb') as f:
+                scaler = pickle.load(f)
+
+            input_data = np.array([[lln,ln,swept,mach,aoa]])
+            input_data_scaled = scaler.transform(input_data)
+        
+        elif mode == "Von-Karman Nose":
+            model_cl_path = os.path.join(MODEL_DIR, "Von-Karman Nose_cl.h5")
+            model_cd_path = os.path.join(MODEL_DIR, "Von-Karman Nose_cd.h5")
+            scaler_path = os.path.join(MODEL_DIR, "Von-Karman Nose.pkl")
+
+            model_cl = tf.keras.models.load_model(model_cl_path)
+            model_cd = tf.keras.models.load_model(model_cd_path)
+
+            with open(scaler_path,'rb') as f:
+                scaler = pickle.load(f)
+
+            input_data = np.array([[mach,aoa]])
+            input_data_scaled = scaler.transform(input_data)
+        
+        elif mode == "Missile Shape 1":
+            model_cl_path = os.path.join(MODEL_DIR, "Missile Shape 1_cl.h5")
+            model_cd_path = os.path.join(MODEL_DIR, "Missile Shape 1_cd.h5")
+            scaler_path = os.path.join(MODEL_DIR, "Missile Shape 1.pkl")
+
+            model_cl = tf.keras.models.load_model(model_cl_path)
+            model_cd = tf.keras.models.load_model(model_cd_path)
+
+            with open(scaler_path,'rb') as f:
+                scaler = pickle.load(f)
+
+            input_data = np.array([[mach,aoa]])
+            input_data_scaled = scaler.transform(input_data)
+
+        elif mode == "Missile Shape 2":
+            model_cl_path = os.path.join(MODEL_DIR, "Missile Shape 2_cl.h5")
+            model_cd_path = os.path.join(MODEL_DIR, "Missile Shape 2_cd.h5")
+            scaler_path = os.path.join(MODEL_DIR, "Missile Shape 2.pkl")
+
+            model_cl = tf.keras.models.load_model(model_cl_path)
+            model_cd = tf.keras.models.load_model(model_cd_path)
+
+            with open(scaler_path,'rb') as f:
+                scaler = pickle.load(f)
+            input_data = np.array([[mach,aoa]])
+            input_data_scaled = scaler.transform(input_data)
+
+        elif mode == "Missile Shape 3":
+            model_cl_path = os.path.join(MODEL_DIR, "Missile Shape 3_cl.h5")
+            model_cd_path = os.path.join(MODEL_DIR, "Missile Shape 3_cd.h5")
+            scaler_path = os.path.join(MODEL_DIR, "Missile Shape 3.pkl")
+            
+            model_cl = tf.keras.models.load_model(model_cl_path)
+            model_cd = tf.keras.models.load_model(model_cd_path)
+
+            with open(scaler_path,'rb') as f:
+                scaler = pickle.load(f)
+
+            input_data = np.array([[mach,aoa]])
+            input_data_scaled = scaler.transform(input_data)
+        else: 
+            return jsonify({"error": f"Unsupported mode: {mode}"}), 400
+
+
+        cl_pred = float(model_cl.predict(input_data_scaled)[0][0])
+        cd_pred = float(model_cd.predict(input_data_scaled)[0][0])
+
+        return jsonify({
+            "cl": round(cl_pred, 5),
+            "cd": round(cd_pred, 5)
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ script.js
@@ -0,0 +1,114 @@
+function updateBodyLength() {
+  var length = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--variable-collection-length'));
+  var Ln = parseFloat(document.getElementById('lnInput').value);
+  if (isNaN(length) || isNaN(Ln)) {
+      document.getElementById('lengthInput').value = '';
+  } else {
+      var bodyLength = length - Ln;
+      document.getElementById('lengthInput').value = bodyLength.toFixed(2);
+  }
+}
+
+document.getElementById('lnInput').addEventListener('input', updateBodyLength);
+updateBodyLength();
+
+const dropdownBtn = document.getElementById("dropdownBtn");
+const dropdownList = document.getElementById("dropdownList");
+const body = document.body;
+
+dropdownBtn.addEventListener("click", () => {
+  dropdownList.style.display = dropdownList.style.display === "block" ? "none" : "block";
+});
+
+dropdownList.querySelectorAll("a").forEach(item => {
+  item.addEventListener("click", e => {
+    e.preventDefault();
+    const mode = item.dataset.mode;
+    body.setAttribute("data-variable-collection-mode", mode);
+    dropdownBtn.textContent = `Model: ${mode}`;
+    dropdownList.style.display = "none";
+    updateDisplayedValues();
+    updateFieldLocking(mode);
+  });
+});
+
+document.addEventListener("click", e => {
+  if (!e.target.closest(".dropdown")) dropdownList.style.display = "none";
+});
+
+function updateDisplayedValues() {
+  const cssVars = getComputedStyle(body);
+  const getVar = name => parseFloat(cssVars.getPropertyValue(name))?.toFixed(2) || "0.00";
+
+  const values = {
+    length: getVar("--variable-collection-length"),
+    diameter: getVar("--variable-collection-diameter"),
+    LW: getVar("--variable-collection-LW"),
+    LF: getVar("--variable-collection-LF"),
+    CTW: getVar("--variable-collection-CTW"),
+    CRW: getVar("--variable-collection-CRW"),
+    CTF: getVar("--variable-collection-CTF"),
+    CRF: getVar("--variable-collection-CRF"),
+    BW: getVar("--variable-collection-BW"),
+    BF: getVar("--variable-collection-BF"),
+  };
+
+  document.querySelectorAll(".text-wrapper-3")[0].textContent = values.length;
+  document.querySelectorAll(".text-wrapper-3")[1].textContent = values.LF;
+  document.querySelectorAll(".text-wrapper-3")[2].textContent = values.LW;
+  document.querySelector(".text-wrapper-6").textContent = values.diameter;
+  document.querySelectorAll(".text-wrapper-8")[0].textContent = values.CTW;
+  document.querySelectorAll(".text-wrapper-6")[1].textContent = values.CRW;
+  document.querySelectorAll(".text-wrapper-8")[1].textContent = values.CTF;
+  document.querySelectorAll(".text-wrapper-8")[2].textContent = values.CRF;
+  document.querySelectorAll(".text-wrapper-8")[3].textContent = values.BW;
+  document.querySelectorAll(".text-wrapper-8")[4].textContent = values.BF;
+}
+
+updateDisplayedValues();
+
+document.querySelector('.overlap-3').addEventListener('click', async () => {
+  try {
+    const mach = parseFloat(document.getElementById('machInput').value);
+    const aoa = parseFloat(document.getElementById('aoaInput').value);
+    const ln = parseFloat(document.getElementById('lnInput').value);
+    const swept = parseFloat(document.getElementById('sweptInput').value);
+    const lln = parseFloat(document.getElementById('lengthInput').value);
+    const mode = document.getElementById('dropdownBtn').textContent.split(": ")[1];
+
+    if (isNaN(mach) || isNaN(aoa) || isNaN(ln)|| isNaN(swept)|| isNaN(lln)) {
+      alert("Vui lòng nhập đủ cả 5 thông số!");
+      return;
+    }
+
+    const response = await fetch("http://127.0.0.1:5000/predict", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({mode, mach, aoa, ln, swept, lln})
+    });
+
+    if (!response.ok) throw new Error(`HTTP ${response.status}`);
+
+    const data = await response.json();
+    document.querySelector(".text-wrapper-18").textContent = data.cl;
+    document.querySelector(".text-wrapper-16").textContent = data.cd;
+  } catch (err) {
+    console.error("Lỗi:", err);
+    alert("Không kết nối được với server: " + err.message);
+  }
+});
+
+function updateFieldLocking(mode) {
+  const lnInput = document.getElementById("lnInput");
+  const sweptInput = document.getElementById("sweptInput");
+
+  const isNASA = mode === "NASA";
+
+  lnInput.readOnly = !isNASA;
+  sweptInput.readOnly = !isNASA;
+
+  if (!isNASA) {
+    lnInput.value = 19.32;
+    sweptInput.value = 16.54;
+  }
+}
